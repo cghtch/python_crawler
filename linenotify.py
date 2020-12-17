802e85761ac5da2d294c03dc4c0f058d53baf4bf
cghtch <kiwi99213001@gmail.com>
2020-12-17 17:12:26 +0800

    Update linenotify.py

--- linenotify.py
+++ linenotify.py
@@ -5,7 +5,7 @@ stock = scraper.Stock("2330")
 result = stock.scrape()
 price = result[0][3]
 
-headers = {"Authorization": "Bearer " + "fJYb5WObFDm8VGJr0LWAX7HGMfoQhkU5PuUR2SZLgQO",
+headers = {"Authorization": "Bearer " + "Line Notify Token",
            "Content-Type": "application/x-www-form-urlencoded"}
 params = {"message": "2330台積電已降價至" + price + "元"}
 if int(price) < 600:  # 將爬取的價格字串轉型為整數
